f34374a4ef565be647115ba5817adbc65eda8d85
13769450144 <13769450144@163.com>
2026-08-11 15:57:56 +0800

    初始化

---
+++ src/stores/user.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+
+interface UserInfo {
+    name: string
+    age: number
+}
+
+// 定义用户模块的store
+export const useUserStore = defineStore('user', {   
+    state: () => ({
+        name: '',
+        age: 0,
+    }),
+    getters: {
+        getName(state) {
+            return state.name
+        },
+        getAge(state) {
+            return state.age
+        },
+    },
+    actions: {
+        setName(name: string) {
+            this.name = name
+        },
+        setAge(age: number) {
+            this.age = age
+        },
+    },
+})
---
+++ vite.config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+// import ElementPlus from 'unplugin-element-plus/vite'
+// import ElementPlusResolver from 'unplugin-vue-components/resolvers/element-plus'
+
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    // 自动按需加载elementplus组件
+    // ElementPlus({}),
+    // AutoImport({
+    //   resolvers: [
+    //     ElementPlusResolver({
+    //       importStyle: 'sass',
+    //     }),
+    //   ],
+    // }),
+    // Components({
+    //   dts: false,
+    //   resolvers: [
+    //     ElementPlusResolver({
+    //       importStyle: 'sass',
+    //       themes: ['dark'],
+    //     }),
+    //   ],
+    // }),
+    vueDevTools({
+      // 1. 组件检查器（点击页面元素定位组件，默认开启）
+      componentInspector: {
+        toggleButtonVisibility: 'never' // always/never，悬浮按钮是否显示
+      },
+
+      // 2. 点击组件时打开的编辑器，默认 code (VSCode)
+      launchEditor: 'code', // code / idea / webstorm / codium
+
+      // 3. 非html入口项目（如SSR）专用，注入脚本到指定文件
+      appendTo: 'src/main.ts',
+
+      // 4. 生产环境强制关闭（默认true，仅dev启用）
+      disable: process.env.NODE_ENV === 'production'
+    }),
+    vue(),],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+})
